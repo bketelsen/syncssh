ad5c854499af3d32cf3c65527564a97eba6a844e
Brian Ketelsen <bketelsen@gmail.com>
2021-11-05 06:45:54 -0400

    first post

--- ssh/config.go
+++ ssh/config.go
@@ -11,14 +11,14 @@ type ClientConfig struct {
 
 func (c *ClientConfig) String() string {
 	sb := &strings.Builder{}
-	sb.WriteString("Host: ")
+	sb.WriteString("Host ")
 	sb.WriteString(c.Host)
 	sb.WriteString("\n")
-	sb.WriteString("  HostName: ")
+	sb.WriteString("  HostName ")
 	sb.WriteString(c.HostName)
 	sb.WriteString("\n")
 	if c.User != "" {
-		sb.WriteString("  User: ")
+		sb.WriteString("  User ")
 		sb.WriteString(c.User)
 		sb.WriteString("\n")
 	}
