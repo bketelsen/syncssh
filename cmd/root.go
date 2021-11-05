1601d7e67295038931f7d29e268f24f401c4f003
Brian Ketelsen <bketelsen@gmail.com>
2021-11-05 06:58:54 -0400

    docs

--- cmd/root.go
+++ cmd/root.go
@@ -35,13 +35,10 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "syncssh",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Synchronize your ssh config file with LXD instance list",
+	Long: `syncssh reads the list of LXC instances from your configured
+LXD remote and adds them to ~/.ssh/config.lxd, which you can import into
+your ssh config file.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
--- cmd/sync.go
+++ cmd/sync.go
@@ -30,13 +30,10 @@ import (
 // syncCmd represents the sync command
 var syncCmd = &cobra.Command{
 	Use:   "sync",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "synchronize configs",
+	Long: `syncssh reads the list of LXC instances from your configured
+	LXD remote and adds them to ~/.ssh/config.lxd, which you can import into
+	your ssh config file.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		/*fmt.Println("tailscale:", viper.GetBool("tailscale"))
 		fmt.Println("lxd:", viper.GetBool("lxd"))
